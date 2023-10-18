f871efcd49ecb1627bc2ee1ec43b11b3fdba9727
arvidj <arvidj89@gmail.com>
2023-10-18 22:54:30 +0200

    oppdaterte stuff

--- 1im/app.py
+++ 1im/app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_login import UserMixin
-from flask_login import login_user, current_user
+from flask_login import login_user, current_user, login_required, logout_user
 from sqlalchemy.exc import IntegrityError as SQLIntegrityError
 from werkzeug.security import generate_password_hash, check_password_hash
 
@@ -49,7 +49,7 @@ def profile():
     context.update({
         'active_tab' : 'profile',
         'user' : current_user,
-        'userdata' : vars(current_user),
+        'userdata' : vars(current_user), # testing
     })
     return render_template('profile.html', **context)
 
@@ -57,7 +57,10 @@ def profile():
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     context = CONTEXT.copy()
-    context['active_tab'] = 'register'
+    context.update({
+        'active_tab':'register',
+        'user' : current_user,
+    })
 
     if request.method == 'POST':
 
@@ -73,18 +76,21 @@ def register():
         except SQLIntegrityError as e:
             # User already exists
             flash('Sorry but that username already exists!','warning')
-            return redirect(url_for('login'))
+            return redirect(url_for('register'))
         
         flash('Account created successfully!', 'success')
         
         return redirect(url_for('login'))
-    return render_template('register.html')
+    return render_template('register.html',**context)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
 
     context = CONTEXT.copy()
-    context['active_tab'] = 'register'
+    context.update({
+        'active_tab':'login',
+        'user' : current_user,
+    })
 
     if request.method == 'POST':
         username = request.form['username']
@@ -92,19 +98,24 @@ def login():
         user = User.query.filter_by(username=username).first()
         password_valid = check_password_hash(user.password, password)
         if user and password_valid:
-            flash('Login successful!', 'success')
+            flash(f'Innloggingen var vellykket, velkommen tilbake {user}!', 'success')
+            flash(f'Vi setter pris på ditt medlemskap', 'warning')
+            flash(f'Betal mere penger ellers smeller det!', 'danger')
+            
             login_user(user)  # Log the user in
+            
             return redirect(url_for('index'))
         else:
             flash('Invalid username or password. Please try again.', 'error')
-    return render_template('login.html')
-"""
-@app.route("/logout")
+    return render_template('login.html',**context)
+
 @login_required
+@app.route("/logout")
 def logout():
     logout_user()
-    return redirect(somewhere)
-"""
+    flash("Du ble logget ut","success")
+    return redirect('/')
+
 
 
 if __name__ == '__main__':
