b2de9ab257fa886c4aae74f226ebf8e75de6d7fc
arvidj <arvidj89@gmail.com>
2023-10-18 21:31:19 +0200

    oppdaterte stuff

--- 1im/app.py
+++ 1im/app.py
@@ -1,44 +1,70 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from flask_login import UserMixin
+from flask_login import login_user, current_user
 from sqlalchemy.exc import IntegrityError as SQLIntegrityError
 from werkzeug.security import generate_password_hash, check_password_hash
+
+CONTEXT = { # put settings here
+    'site_title' : 'Arvids morohule',
+    'welcome_msg' : 'Hei og velkommen til Arvids morohule',
+    'default_profile_pic' : '/media/default_profile_pic.png',
+    'allow_anonymous_users' : True,
+           }
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'your_secret_key_here'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
 
-# from flask import 
-
+login_manager = LoginManager(app)
 db = SQLAlchemy(app)
 
-class User(db.Model):
+
+
+class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     password = db.Column(db.String(120), nullable=False)
 
-    def say_hello(self):
-        return "Hello from " + self.username
+    def __repr__(self):
+        return str(self.username)
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
 @app.route('/')
 def index():
-    nav_target = 'index'
-    return render_template('index.html')
+    context = CONTEXT.copy()
+    context.update({
+        'active_tab':'index',
+        'user' : current_user,
+    })
+    return render_template('index.html', **context)
 
 @app.route('/profile', methods=['GET', 'POST'])
 def profile():
-    raise Exception
-    return render_template('profile.html', request=request, active_tab="profile")
+    context = CONTEXT.copy()
+    context.update({
+        'active_tab' : 'profile',
+        'user' : current_user,
+        'userdata' : vars(current_user),
+    })
+    return render_template('profile.html', **context)
 
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
-    
+    context = CONTEXT.copy()
+    context['active_tab'] = 'register'
+
     if request.method == 'POST':
+
         username = request.form['username']
         password = request.form['password']
-        hashed_password = generate_password_hash(password, 
-                                                 # method='sha256'
-                                                 )
+
+        hashed_password = generate_password_hash(password) 
         new_user = User(username=username, password=hashed_password)
         db.session.add(new_user)
         
@@ -56,7 +82,10 @@ def register():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    active_navbar_element = 'login'
+
+    context = CONTEXT.copy()
+    context['active_tab'] = 'register'
+
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
@@ -64,6 +93,7 @@ def login():
         password_valid = check_password_hash(user.password, password)
         if user and password_valid:
             flash('Login successful!', 'success')
+            login_user(user)  # Log the user in
             return redirect(url_for('index'))
         else:
             flash('Invalid username or password. Please try again.', 'error')
